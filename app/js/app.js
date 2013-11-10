9ff4459589356430bce56ae04af8b926a68bbd23
John Walley <john@walley.org.uk>
2013-11-10 20:27:12 +0000

    Copied across current plnkr files

--- app/js/app.js
+++ app/js/app.js
@@ -2,15 +2,145 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+  var riskJargonGenerator = angular.module('riskJargonGenerator', ['ui.router', 'ui.bootstrap', "ParseServices", "ExternalDataServices"]);
+
+  riskJargonGenerator.factory('Jargon', function() {
+
+    var constructs = [{
+      types: ["verb", "adjective", "abbreviation", "noun"],
+      structure: "We need to {0} the {1} {2} {3}!"
+    }, {
+      types: ["verb", "abbreviation", "noun", "verb", "adjective", "noun"],
+      structure: "Try to {0} the {1} {2} - maybe it will {3} the {4} {5}!"
+    }];
+
+    var jargon = {};
+
+    jargon.constructs = constructs;
+
+    jargon.generate = function(x) {
+      var construct = jargon.constructs[Math.floor(Math.random() * jargon.constructs.length)];
+      var sentence = construct.structure;
+
+      jargon.verbs = _.map(x.verbs.models, function(m) {
+        return m.getWord()
+      });
+      jargon.abbreviations = _.map(x.abbreviations.models, function(m) {
+        return m.getWord()
+      });
+      jargon.nouns = _.map(x.nouns.models, function(m) {
+        return m.getWord()
+      });
+      jargon.adjectives = _.map(x.adjectives.models, function(m) {
+        return m.getWord()
+      });
+
+      for (var i = 0; i < construct.types.length; i++) {
+
+        var words = jargon[construct.types[i] + "s"];
+        var wordIndex = Math.floor(Math.random() * words.length);
+
+        var word = words[wordIndex];
+
+        sentence = sentence.replace("{" + i + "}", word);
+      }
+
+      return sentence;
+    }
+
+    return jargon
+  });
+
+  riskJargonGenerator.config(function($stateProvider, $urlRouterProvider) {
+
+    // For any unmatched url, send to /jargon
+    $urlRouterProvider.otherwise("/jargon")
+
+    $stateProvider
+      .state('jargon', {
+        url: "/jargon",
+        templateUrl: "partials/jargon.html",
+        resolve: {
+          'jargon': ['JargonService',
+            function(JargonService) {
+
+              // Get the collections from our data definitions
+              var verbs = new JargonService.verbs();
+              var abbreviations = new JargonService.abbreviations();
+              var nouns = new JargonService.nouns();
+              var adjectives = new JargonService.adjectives();
+              var sentences = new JargonService.sentences();
+
+              // Use the extended Parse SDK to load the whole collection
+              verbs.load();
+              abbreviations.load();
+              nouns.load();
+              adjectives.load();
+              sentences.load();
+
+              return {
+                verbs: verbs,
+                abbreviations: abbreviations,
+                nouns: nouns,
+                adjectives: adjectives,
+                sentences: sentences
+              }
+            }
+          ]
+        },
+        controller: function($scope, Jargon, jargon) {
+          $scope.isCollapsed = true;
+
+          $scope.refresh = function() {
+            $scope.sentence = Jargon.generate(jargon);
+          }
+
+          $scope.favourite = function() {
+            jargon.sentences.addSentence($scope.sentence);
+          }
+
+          Jargon.generate(jargon);
+        }
+      })
+      .state('jargon.list', {
+        url: "/list",
+        templateUrl: "partials/jargon.list.html",
+        controller: function($scope, jargon) {
+
+          $scope.verbs = jargon.verbs.models;
+          $scope.abbreviations = jargon.abbreviations.models;
+          $scope.nouns = jargon.nouns.models;
+          $scope.adjectives = jargon.adjectives.models;
+
+          $scope.addVerb = function() {
+            jargon.verbs.addWord($scope.newverb)
+          };
+
+          $scope.addAbbreviation = function() {
+            jargon.abbreviations.addWord($scope.newabbreviation)
+          };
+
+          $scope.addNoun = function() {
+            jargon.nouns.addWord($scope.newnoun)
+          };
+
+          $scope.addAdjective = function() {
+            jargon.adjectives.addWord($scope.newadjective)
+          };
+        }
+      })
+      .state('about', {
+        url: "/about",
+        templateUrl: "partials/about.html"
+      })
+
+  });
+
+  riskJargonGenerator.run(['ParseSDK', 'ExtendParseSDK', '$rootScope', '$state', '$stateParams',
+    function(ParseService, ExtendParseSDK, $rootScope, $state, $stateParams) {
+
+      // Parse is initialised by injecting the ParseService into the Angular app
+      $rootScope.$state = $state;
+      $rootScope.$stateParams = $stateParams;
+    }
+  ]);
--- app/js/services.js
+++ app/js/services.js
@@ -1,9 +1,356 @@
 'use strict';
 
 /* Services */
+angular.module('ParseServices', []).
+  factory('ExtendParseSDK', ['ParseAbstractService',
+  function(ParseAbstractService) {
+
+    Parse.Object.extendAngular = function(options) {
+      return ParseAbstractService.EnhanceObject(Parse.Object.extend(options));
+    };
+
+    Parse.Collection.extendAngular = function(options) {
+      return ParseAbstractService.EnhanceCollection(Parse.Collection.extend(options));
+    };
+
+  }
+]).
+  factory('ParseSDK', function() {
+
+  // pro-tip: swap these keys out for PROD keys automatically on deploy using grunt-replace
+  Parse.initialize("prP3hizyid9MPKOEZaus97jg0GbNCUkTJd44Elai", "qMn3rd66bA8eLyuYPLJP4I3ZNo5qvVklNUOKH0Bp");
+});
+
+angular.module('ParseServices')
+
+.factory('ParseQueryAngular',['$q','$timeout',function ($q, $timeout) { 
 
 
-// Demonstrate how to register services
-// In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+  // we use $timeout 0 as a trick to bring resolved promises into the Angular digest
+  var angularWrapper = $timeout;
+
+	return function(query,options) {
+
+      // if unspecified, the default function to call is 'find'
+      var functionToCall = "find";
+      if (!_.isUndefined(options) && !_.isUndefined(options.functionToCall)) {
+        functionToCall = options.functionToCall;
+      }
+
+      // create a promise to return
+      var defer = $q.defer();
+      
+      // this is the boilerplate stuff that you would normally have to write for every Parse call
+      var defaultParams = [{
+        success:function(data) {
+
+          /* We're using $timeout as an "angular wrapper" that will force a digest
+          * and kind of bring back the data in Angular realm.
+          * You could use the classic $scope.$apply as well but here we don't need
+          * to pass any $scope as a parameter.
+          * Another trick is to inject $rootScope and use $apply on it, but well, $timeout is sexy.
+          */
+          angularWrapper(function(){
+            defer.resolve(data);
+          });
+        },
+        error:function(data,err) {
+          angularWrapper(function(){
+            defer.reject(err);
+          });
+        }
+      }];
+      // Pass custom params if needed.
+      if (options && options.params) {
+        defaultParams = options.params.concat(defaultParams);
+      }
+      if (options && options.mergeParams) {
+        defaultParams[0] = _.extend(defaultParams[0],options.mergeParams);
+      }
+
+      // this is where the async call is made outside the Angular digest
+      query[functionToCall].apply(query,defaultParams);
+
+      return defer.promise;
+
+    };
+
+}]);  
+
+angular.module('ExternalDataServices',[]) 
+
+.factory('ParseAbstractService', ['ParseQueryAngular', function(ParseQueryAngular) {
+	/********
+		This service provides an enhanced Parse.Object and Parse.Collection
+		So we can call load and saveParse from any extending Class and have that wrapped
+		within ParseQueryAngular
+	**********/
+
+
+	var object = function(originalClass) {
+		originalClass.prototype = _.extend(originalClass.prototype,{
+			load:function() {
+				return ParseQueryAngular(this,{functionToCall:"fetch"});
+			},
+			saveParse:function(data) {
+				if (data && typeof data == "object") this.set(data);
+				return ParseQueryAngular(this,{functionToCall:"save", params:[null]});
+			}
+		});
+		return originalClass;
+	};
+
+	var collection = function(originalClass){
+		originalClass.prototype = _.extend(originalClass.prototype,{
+			load:function() {
+				return ParseQueryAngular(this,{functionToCall:"fetch"});
+			}
+		});
+		return originalClass;
+	};
+
+
+	return {
+		EnhanceObject:object,
+		EnhanceCollection:collection
+	};
+
+}]);
+
+// reference the module we declared earlier
+angular.module('ExternalDataServices')
+
+// add a factory
+.factory('JargonService', ['ParseQueryAngular',
+  function(ParseQueryAngular) {
+
+    var Sentence = Parse.Object.extendAngular({
+      className: "Favourites",
+      setSentence: function(sentence) {
+        this.set('sentence', sentence);
+        return this;
+      },
+      getSentence: function() {
+        return this.get('sentence');
+      },
+      setVotes: function(votes) {
+        this.set('votes', votes);
+        return this;
+      },
+      getVotes: function() {
+        return this.get('votes');
+      },
+      destroyParse: function() {
+        return ParseQueryAngular(this, {
+          functionToCall: "destroy"
+        });
+      }
+    });
+
+    var Verb = Parse.Object.extendAngular({
+      className: "Verb",
+      setWord: function(word) {
+        this.set('word', word);
+        return this;
+      },
+      getWord: function() {
+        return this.get('word');
+      },
+      destroyParse: function() {
+        return ParseQueryAngular(this, {
+          functionToCall: "destroy"
+        });
+      }
+    });
+
+    var Abbreviation = Parse.Object.extendAngular({
+      className: "Abbreviation",
+      setWord: function(word) {
+        this.set('word', word);
+        return this;
+      },
+      getWord: function() {
+        return this.get('word');
+      },
+      destroyParse: function() {
+        return ParseQueryAngular(this, {
+          functionToCall: "destroy"
+        });
+      }
+    });
+
+    var Noun = Parse.Object.extendAngular({
+      className: "Noun",
+      setWord: function(word) {
+        this.set('word', word);
+        return this;
+      },
+      getWord: function() {
+        return this.get('word');
+      },
+      destroyParse: function() {
+        return ParseQueryAngular(this, {
+          functionToCall: "destroy"
+        });
+      }
+    });
+
+    var Adjective = Parse.Object.extendAngular({
+      className: "Adjective",
+      setWord: function(word) {
+        this.set('word', word);
+        return this;
+      },
+      getWord: function() {
+        return this.get('word');
+      },
+      destroyParse: function() {
+        return ParseQueryAngular(this, {
+          functionToCall: "destroy"
+        });
+      }
+    });
+
+    var Sentences = Parse.Collection.extendAngular({
+      model: Sentence,
+      comparator: function(model) {
+        return -model.createdAt.getTime();
+      },
+      addSentence: function(sentenceIn) {
+        // save request_id to Parse
+        var _this = this;
+
+        var sentence = new Sentence();
+        sentence.setSentence(sentenceIn);
+
+        // use the extended Parse SDK to perform a save and return the promised object back into the Angular world
+        return sentence.saveParse().then(function(data) {
+          _this.add(data);
+        })
+      },
+      removeWord: function(sentence) {
+        if (!this.get(sentence)) return false;
+        var _this = this;
+        return sentence.destroyParse().then(function() {
+          _this.remove(sentence);
+        });
+      }
+    });
+
+    var Verbs = Parse.Collection.extendAngular({
+      model: Verb,
+      comparator: function(model) {
+        return -model.createdAt.getTime();
+      },
+      addWord: function(word) {
+        // save request_id to Parse
+        var _this = this;
+
+        var verb = new Verb();
+        verb.setWord(word);
+
+        // use the extended Parse SDK to perform a save and return the promised object back into the Angular world
+        return verb.saveParse().then(function(data) {
+          _this.add(data);
+        })
+      },
+      removeWord: function(word) {
+        if (!this.get(word)) return false;
+        var _this = this;
+        return word.destroyParse().then(function() {
+          _this.remove(word);
+        });
+      }
+    });
+
+    var Abbreviations = Parse.Collection.extendAngular({
+      model: Abbreviation,
+      comparator: function(model) {
+        return -model.createdAt.getTime();
+      },
+      addWord: function(word) {
+        // save request_id to Parse
+        var _this = this;
+
+        var abbreviation = new Abbreviation();
+        abbreviation.setWord(word);
+
+        // use the extended Parse SDK to perform a save and return the promised object back into the Angular world
+        return abbreviation.saveParse().then(function(data) {
+          _this.add(data);
+        })
+      },
+      removeWord: function(word) {
+        if (!this.get(word)) return false;
+        var _this = this;
+        return word.destroyParse().then(function() {
+          _this.remove(word);
+        });
+      }
+    });
+
+    var Nouns = Parse.Collection.extendAngular({
+      model: Noun,
+      comparator: function(model) {
+        return -model.createdAt.getTime();
+      },
+      addWord: function(word) {
+        // save request_id to Parse
+        var _this = this;
+
+        var noun = new Noun();
+        noun.setWord(word);
+
+        // use the extended Parse SDK to perform a save and return the promised object back into the Angular world
+        return noun.saveParse().then(function(data) {
+          _this.add(data);
+        })
+      },
+      removeWord: function(word) {
+        if (!this.get(word)) return false;
+        var _this = this;
+        return word.destroyParse().then(function() {
+          _this.remove(word);
+        });
+      }
+    });
+
+    var Adjectives = Parse.Collection.extendAngular({
+      model: Adjective,
+      comparator: function(model) {
+        return -model.createdAt.getTime();
+      },
+      addWord: function(word) {
+        // save request_id to Parse
+        var _this = this;
+
+        var adjective = new Adjective();
+        adjective.setWord(word);
+
+        // use the extended Parse SDK to perform a save and return the promised object back into the Angular world
+        return adjective.saveParse().then(function(data) {
+          _this.add(data);
+        })
+      },
+      removeWord: function(word) {
+        if (!this.get(word)) return false;
+        var _this = this;
+        return word.destroyParse().then(function() {
+          _this.remove(word);
+        });
+      }
+    });
+
+
+    // Return a simple API
+    return {
+      verbs: Verbs,
+      abbreviations: Abbreviations,
+      nouns: Nouns,
+      adjectives: Adjectives,
+      sentences: Sentences
+    };
+  }
+]);
+
+
