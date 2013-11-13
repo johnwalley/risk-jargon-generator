9e5ecf81cf645acc78f06fa1c9c0f181607d3919
John Walley <github@walley.org.uk>
2013-11-13 13:08:56 +0000

    Added 'version' service

--- app/js/services.js
+++ app/js/services.js
@@ -1,6 +1,10 @@
 'use strict';
 
 /* Services */
+
+angular.module('myApp.services', []).
+  value('version', '0.1');
+
 angular.module('ParseServices', []).
   factory('ExtendParseSDK', ['ParseAbstractService',
   function(ParseAbstractService) {
