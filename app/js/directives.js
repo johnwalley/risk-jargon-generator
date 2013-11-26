8cb7ef5cc4db286e235aec8857e12da2444e1f99
John Walley <john@walley.org.uk>
2013-11-26 21:09:06 +0000

    include directives

--- app/js/directives.js
+++ app/js/directives.js
@@ -2,7 +2,7 @@
 
 /* Directives */
 
-angular.module('riskJargonGenerator.directives', ['riskJargonGenerator.services']).
+angular.module('riskJargonGenerator.directives', []).
   directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
