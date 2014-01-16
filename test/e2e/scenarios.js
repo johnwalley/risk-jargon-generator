f8bde89e28593c95afe4cd6304a535e2791789df
John Walley <john.walley@john.walley>
2014-01-16 13:46:48 +0000

    Remove e2e tests which do nothing

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -12,34 +12,4 @@ describe('my app', function() {
   it('should automatically redirect to /jargon when location hash/fragment is empty', function() {
     expect(browser().location().url()).toBe("/jargon");
   });
-
-
-  describe('jargon', function() {
-
-    beforeEach(function() {
-      browser().navigateTo('#/jargon');
-    });
-
-
-    it('should render jargon when user navigates to /jargon', function() {
-      expect(element('.btn').text()).
-        toMatch(/partial for view 1/);
-    });
-
-  });
-
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser().navigateTo('#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 2/);
-    });
-
-  });
 });
