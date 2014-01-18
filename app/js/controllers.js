3e9cc7a635510ab3f155e4e30263dfb609ec88a0
John Walley <john@walley.org.uk>
2014-01-18 11:18:56 +0000

    Remove jargon list ui-view and instead have it behing a simple collapsible

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -29,19 +29,6 @@ controller('JargonCtrl', ['$scope', 'Jargon', 'JargonService', '$q', function($s
 
   // Wait for all promises to resolve before creating initial jargon.
   $q.all([verbs.load(), abbreviations.load(), nouns.load(), adjectives.load()]).then( function() { $scope.sentence = Jargon.generate(jargon)} );
-}]).
-controller('JargonListCtrl', ['$scope', 'JargonService', function($scope, JargonService) {
-
-  // Get the collections from our data definitions
-  var verbs = new JargonService.verbs();
-  var abbreviations = new JargonService.abbreviations();
-  var nouns = new JargonService.nouns();
-  var adjectives = new JargonService.adjectives();
-
-  verbs.load();
-  abbreviations.load();
-  nouns.load();
-  adjectives.load();
 
   $scope.verbs = verbs;
   $scope.abbreviations = abbreviations;
